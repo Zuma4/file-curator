e5f3ea260140ad76a81edbbc47c9e3f94a807b28
Zuma4 <135827801+Zuma4@users.noreply.github.com>
2023-12-12 18:48:51 +0200

    Update build.bat

--- source/build.bat
+++ source/build.bat
@@ -1,1 +1,1 @@
-g++ string_manipulation/string_manipulation.cpp file_management/file_management.cpp cmd/cmd.cpp unit_testing/unit_testing.cpp main.cpp -o bin
+g++ string_manipulation/string_manipulation.cpp file_management/file_management.cpp CUI/CUI.cpp unit_testing/unit_testing.cpp main.cpp -o bin
